c3fd7ddc10a9313abdad3195b529112c37e86d7a
James Carter <jamesmcarter7@gmail.com>
2021-01-09 16:37:09 +0000

    Initial Commit

---
+++ MediaOrganiser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MediaOrganiser
+//
+//  Created by James on 09/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MediaOrganiser/MediaOrganiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaOrganiserApp.swift
+//  MediaOrganiser
+//
+//  Created by James on 09/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct MediaOrganiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
