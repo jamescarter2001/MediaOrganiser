185272af29d06314be812b868cb91a59350cdac9
James Carter <jamesmcarter7@gmail.com>
2021-01-14 17:58:09 +0000

    Full json integration + UI improvements

---
+++ MediaOrganiser/src/browser/group/GroupBrowserContainerView.swift
@@ -0,0 +1,20 @@
+//
+//  GroupBrowserContainerView.swift
+//  MediaOrganiser
+//
+//  Created by James on 14/01/2021.
+//
+
+import SwiftUI
+
+struct GroupBrowserContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GroupBrowserContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupBrowserContainerView()
+    }
+}
---
+++ MediaOrganiser/src/util/JSONBrowserFileHandler.swift
@@ -0,0 +1,74 @@
+//
+//  JSONCoder.swift
+//  MediaOrganiser
+//
+//  Created by James on 13/01/2021.
+//
+
+import Foundation
+
+class JSONBrowserFileHandler {
+    
+    static let encoder = JSONEncoder()
+    //let decoder = JSONDecoder()
+    
+    static func EncodeAndSaveFile(dict : [String:[BrowserFile]], path : String) {
+        
+        var encodedString : String = ""
+        
+        do {
+            let encoded : Data = try encoder.encode(dict)
+            encodedString = String(data: encoded, encoding: .utf8)!
+        } catch {
+            print("Error encoding array: \(dict)")
+        }
+        
+        let filename = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("/Desktop/output.wzstate")
+        
+        do {
+            try encodedString.write(to: filename, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print("File write error!")
+        }
+        
+        print(encodedString)
+    }
+    
+    static func DecodeFile(path : String) -> [String:[BrowserFile]] {
+        
+        var finalDict : [String : [BrowserFile]] = [:]
+        
+        let encodedPath = path.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let pathUrl = URL(string: encodedPath!)!
+        var testUrl = URLComponents()
+        testUrl.scheme = "file"
+        testUrl.path = path
+        
+        do {
+            
+            let data = try String(contentsOf: testUrl.url!).data(using: .utf8)!
+            if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                if let test = json["AAA"] as? [[String:Any]] {
+                    for key in Array(json.keys) {
+                        
+                        let block = json[key] as! [[String:Any]]
+                        var files : [BrowserFile] = []
+                        
+                        for file in block {
+                            let browserFile : BrowserFile = BrowserFile(name: file["name"] as! String, path: file["path"] as! String, size: file["size"] as! UInt64, type: EFileType(rawValue: file["type"] as! String) ?? EFileType.unknown, group: EFileGroup.none)
+                            
+                            files.append(browserFile)
+                        }
+                        finalDict[key] = files
+                    }
+                } else {
+                    print("parse error")
+                }
+            }
+        } catch {
+            //print("Decode error: \(error)")
+        }
+        print(finalDict)
+        return finalDict
+    }
+}
